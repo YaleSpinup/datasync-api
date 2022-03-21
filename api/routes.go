1a7eb62a1cd09c3df349e99e5a22cc3a5e745066
nvnyale <naveen.akula@yale.edu>
2022-03-21 00:30:06 -0400

    Updated signature of API

--- api/routes.go
+++ api/routes.go
@@ -38,8 +38,8 @@ func (s *server) routes() {
 	api.HandleFunc("/{account}/movers/{group}/{name}", s.MoverDeleteHandler).Methods(http.MethodDelete)
 	// api.HandleFunc("/{account}/movers/{group}/{name}/{id}", s.MoverUpdateHandler).Methods(http.MethodPut)
 
-	api.HandleFunc("/{account}/movers/{group}/{name}/start", s.StartTaskHandler).Methods(http.MethodPost)
-	api.HandleFunc("/{account}/movers/{group}/{name}/stop", s.StopTaskHandler).Methods(http.MethodPut)
+	api.HandleFunc("/{account}/movers/{group}/{name}/runs", s.StartTaskHandler).Methods(http.MethodPost)
+	api.HandleFunc("/{account}/movers/{group}/{name}/runs", s.StopTaskHandler).Methods(http.MethodPut)
 	api.HandleFunc("/{account}/movers/{group}/{name}/startStop", s.StartStopTaskHandler).Methods(http.MethodPut)
 
 	api.HandleFunc("/{account}/movers/{group}/{name}/runs", s.RunListHandler).Methods(http.MethodGet)
