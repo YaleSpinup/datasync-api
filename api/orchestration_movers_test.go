f0b8a0e3e5d1720687d889e2959dad2158436735
nvnyale <naveen.akula@yale.edu>
2022-03-29 13:49:00 -0400

    StartTask Testcases

--- api/orchestration_movers_test.go
+++ api/orchestration_movers_test.go
@@ -17,18 +17,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+//datasyncClient
 type mockDatasync struct {
 	datasynciface.DataSyncAPI
 	t   *testing.T
 	err error
 }
 
+//rgClient
 type mockrgClient struct {
 	resourcegroupstaggingapiiface.ResourceGroupsTaggingAPIAPI
 	t   *testing.T
 	err error
 }
 
+// We dont want to start task so we mocked it
 func (d *mockDatasync) StartTaskExecutionWithContext(ctx context.Context, input *datasync.StartTaskExecutionInput, opts ...request.Option) (*datasync.StartTaskExecutionOutput, error) {
 	fmt.Println("mocked StartTaskExecutionWithContext")
 	if d.err != nil {
@@ -42,8 +45,19 @@ func (d *mockDatasync) StartTaskExecutionWithContext(ctx context.Context, input 
 	return out, nil
 }
 
+var is_running = false
+
+// It returns a task with RUNNING / AVAIABLE bases on is_running
 func (d *mockDatasync) DescribeTaskWithContext(ctx context.Context, input *datasync.DescribeTaskInput, opts ...request.Option) (*datasync.DescribeTaskOutput, error) {
 	fmt.Println("mocked DescribeTaskWithContext")
+	if is_running {
+
+		return &datasync.DescribeTaskOutput{
+			Name:    aws.String("name1"),
+			Status:  aws.String("RUNNING"),
+			TaskArn: aws.String("arn:aws:datasync:us-east-1:516855177326:task/task-05cd6f77d7b5d15ac/execution/exec-086d6c629a6bf3581"),
+		}, nil
+	}
 	return &datasync.DescribeTaskOutput{
 		Name:    aws.String("name1"),
 		Status:  aws.String("AVAILABLE"),
@@ -51,6 +65,7 @@ func (d *mockDatasync) DescribeTaskWithContext(ctx context.Context, input *datas
 	}, nil
 }
 
+//Get tags
 func (r *mockrgClient) GetResourcesWithContext(ctx context.Context, input *resourcegroupstaggingapi.GetResourcesInput, opts ...request.Option) (*resourcegroupstaggingapi.GetResourcesOutput, error) {
 	fmt.Println("mocked GetResourcesWithContext")
 	fmt.Print(ctx, input, opts)
@@ -80,8 +95,8 @@ func newmockrgClient(t *testing.T, err error) resourcegroupstaggingapiiface.Reso
 	}
 }
 
-func Test_StartRun(T *testing.T) {
-
+func Test_StartRun_Runs_When_Status_Available(T *testing.T) {
+	is_running = false
 	o := &datasyncOrchestrator{
 		account: "",
 		server:  &server{},
@@ -100,30 +115,22 @@ func Test_StartRun(T *testing.T) {
 	assert.Equal(T, "exec-086d6c629a6bf3581", resp)
 }
 
-// func Tesst_startTaskRun(T *testing.T) {
-// 	type TaskRunRes struct {
-// 		res string
-// 		err error
-// 	}
-
-// 	cases := []struct {
-// 		ctx      context.Context
-// 		group    string
-// 		name     string
-// 		expected TaskRunRes
-// 		message  string
-// 	}{
-// 		{nil, "Gruup1", "AvialName",
-// 			TaskRunRes{"arn:aws:datasync:us-east-1:516855177326:task/task-05cd6f77d7b5d15ac/execution/exec-086d6c629a6bf3581", nil}, "Should not return full arn"},
-// 		//	{nil, "Gruup1", "RunningName", TaskRunRes{nil, apierror.New(apierror.ErrConflict, "")}, "y greater than x"},
-// 		{nil, "Gruup1", "AvialSuccess", TaskRunRes{"exec-086d6c629a6bf3581", nil}, "Should return a trimmed error"},
-// 		//	{nil, "Gruup1", "Running", TaskRunRes{"", nil}, "negative values, y greater than x"},
-// 	}
-// 	fmt.Println(cases, "cases")
-// 	o := &MockdatasyncOrchestrator{}
-// 	for _, test := range cases {
-// 		resp, err := o.datasyncOrchestrator.startTaskRun(test.ctx, test.group, test.name)
-// 		assert.NotEqual(T, err, nil, "")
-// 		assert.Equal(T, test.expected, resp, test.message)
-// 	}
-// }
+func Test_StartRun_Dose_Not_Run_If_its_running(T *testing.T) {
+	is_running = true
+	o := &datasyncOrchestrator{
+		account: "",
+		server:  &server{},
+		sp:      &sessionParams{},
+		datasyncClient: ydataSync.Datasync{
+			Service:         newmockDatasync(T, nil),
+			DefaultKMSKeyId: "",
+		},
+		iamClient: iam.IAM{},
+		rgClient: yresource.ResourceGroupsTaggingAPI{
+			Service: newmockrgClient(T, nil),
+		},
+	}
+	resp, err := o.startTaskRun(nil, "group1", "name1")
+	assert.Error(T, err, "Conflict: another datasync mover task is already running")
+	assert.Equal(T, "", resp)
+}
